6d42a7feef6e0aee7a08892366a91c0f798b0f8c
İsmail Açıkyürek <ismailackyure@gmail.com>
2021-06-20 19:22:08 +0300

    Initial Commit

---
+++ switch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  switch
+//
+//  Created by İSMAİL AÇIKYÜREK on 20.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
